20078532f8e120e0480016011bdcb7f48f2754a9
Anthony Roscoe <aroscoe@limewire.com>
2010-02-08 19:18:22 -0500

    uploads a single image

--- story/views.py
+++ story/views.py
@@ -4,6 +4,9 @@ from django.views.generic.simple import direct_to_template
 from django.contrib.auth.decorators import login_required
 from story.forms import StoryForm
 from story.models import Story as StoryModel
+from story.models import StoryImage as StoryImageModel
+
+from story import my_debug
 
 @login_required
 def add_story(request):
@@ -11,16 +14,24 @@ def add_story(request):
     form = StoryForm(request.POST)
     
     if form.is_valid():
-      completed_form = form.save(commit=False)
-      completed_form.user = request.user
-      completed_form.save()
-      # image_file = self._handle_uploaded_file(request.FILES['file'])
+      story = form.save(commit=False)
+      
+      # add user name
+      story.user = request.user
+      story.save()
+      
+      # add image 
+      # TODO: change to add multiple images
+      image_file = _handle_uploaded_file(request.FILES['image'])
+      image = StoryImageModel.objects.create(image=image_file)
+      story.images.add(image)
+      
       return render_to_response('success.html')
   else:
     form = StoryForm()
   return direct_to_template(request, 'form.html', locals())
 
-def _handle_uploaded_file(self, file):
+def _handle_uploaded_file(file):
   file_path = settings.UPLOADS_DIR + file.name
   destination = open(file_path, 'wb+')
   for chunk in file.chunks():
