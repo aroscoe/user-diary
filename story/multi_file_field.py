8e497a23da2c34c25e011285909b27d4d05eeb35
Anthony Roscoe <aroscoe@limewire.com>
2010-03-15 17:38:03 -0400

    find replace for multifilefield count arg to num_fields

--- story/multi_file_field.py
+++ story/multi_file_field.py
@@ -12,7 +12,7 @@ class MultiFileInput(FileInput):
     def __init__(self, attrs=None):
         """
         Create a MultiFileInput.
-        The 'count' attribute can be specified to default the number of 
+        The 'num_fields' attribute can be specified to default the number of 
         file boxes initially presented.
         """
         super(MultiFileInput, self).__init__(attrs)
@@ -26,43 +26,43 @@ class MultiFileInput(FileInput):
         js, link, and/or fields methods which provide content to this method.
         """
         final_attrs = self.build_attrs(attrs, type=self.input_type, name=name+'[]')
-        count = final_attrs['count']
-        if count<1: count=1
-        del final_attrs['count']
-        js = self.js(name, value, count, final_attrs)
-        link = self.link(name, value, count, final_attrs)
-        fields = self.fields(name, value, count, final_attrs)
+        num_fields = final_attrs['num_fields']
+        if num_fields<1: num_fields=1
+        del final_attrs['num_fields']
+        js = self.js(name, value, num_fields, final_attrs)
+        link = self.link(name, value, num_fields, final_attrs)
+        fields = self.fields(name, value, num_fields, final_attrs)
         return js+fields+link
     
-    def fields(self, namey, value, count, attrs=None):
+    def fields(self, namey, value, num_fields, attrs=None):
         """
         Renders the necessary number of file input boxes.
         """
-        return u''.join([u'<input%s />\n' % flatatt(dict(attrs, id=attrs['id']+str(i))) for i in range(count)])
+        return u''.join([u'<input%s />\n' % flatatt(dict(attrs, id=attrs['id']+str(i))) for i in range(num_fields)])
     
-    def link(self, name, value, count, attrs=None):
+    def link(self, name, value, num_fields, attrs=None):
         """
         Renders a link to add more file input boxes.
         """
         return u"<a onclick=\"javascript:new_%(name)s()\">+</a>" % {'name':name}
     
-    def js(self, name, value, count, attrs=None):
+    def js(self, name, value, num_fields, attrs=None):
         """
         Renders a bit of Javascript to add more file input boxes.
         """
         return u"""
         <script>
         <!--
-        %(id)s_counter=%(count)d;
+        %(id)s_num_fieldser=%(num_fields)d;
         function new_%(name)s() {
             b=document.getElementById('%(id)s0');
             c=b.cloneNode(false);
-            c.id='%(id)s'+(%(id)s_counter++);
+            c.id='%(id)s'+(%(id)s_num_fieldser++);
             b.parentNode.insertBefore(c,b.parentNode.lastChild.nextSibling);
         }
         -->
         </script>
-        """ % {'id':attrs['id'], 'name':name, 'count':count}
+        """ % {'id':attrs['id'], 'name':name, 'num_fields':num_fields}
     
     def value_from_datadict(self, data, files, name):
         """
@@ -79,18 +79,18 @@ class MultiFileField(Field):
     A field allowing users to upload multiple files at once.
     """
     widget = MultiFileInput
-    count = 1
+    num_fields = 1
     
-    def __init__(self, count=1, *args, **kwargs):
-        self.count = count
+    def __init__(self, num_fields=1, *args, **kwargs):
+        self.num_fields = num_fields
         super(MultiFileField, self).__init__(*args, **kwargs)
     
     def widget_attrs(self, widget):
         """
-        Adds the count to the MultiFileInput widget.
+        Adds the num_fields to the MultiFileInput widget.
         """
         if isinstance(widget, MultiFileInput):
-            return {'count':self.count}
+            return {'num_fields':self.num_fields}
         return {}
     
     def clean(self, data):
@@ -102,7 +102,7 @@ class MultiFileField(Field):
         if not self.required and data in EMPTY_VALUES:
             return None
         
-        if len(data) != self.count:
+        if len(data) != self.num_fields:
             raise ValidationError(ugettext(u"An incorrect number of files were uploaded."))
         
         return data
